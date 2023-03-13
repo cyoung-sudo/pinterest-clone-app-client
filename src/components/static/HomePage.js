de9cc0763f015cbead24cb24dc3ed5fc9d5046d4
Christopher Young <christopheryoung@MacBook123.local>
2023-03-13 14:59:32 -0700

    updated homepage

--- src/components/static/HomePage.js
+++ src/components/static/HomePage.js
@@ -5,6 +5,8 @@ export default function HomePage() {
     <div id="homePage">
       <div id="homePage-header">
         <h1>Pinterest Clone</h1>
+        <p>Upload images and share with other users</p>
+        <p>Signup and get started today</p>
       </div>
     </div>
   );
